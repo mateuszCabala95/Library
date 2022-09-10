aef290c36d6a5b85ca3c419029345052c2886d92
Mateusz Cabała <mateusz.cabala95@gmail.com>
2022-09-10 14:39:00 +0200

    Add books controller

---
+++ Library.Api/Controllers/BooksController.cs
@@ -0,0 +1,53 @@
+﻿using Library.Api.Dto;
+using Library.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController: ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(Seeds.Seeds.Books);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetOne(int id)
+    {
+        var book = Seeds.Seeds.Books.FirstOrDefault(x => x.Id == id);
+
+        if (book is null) return NotFound();
+        return Ok(book);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(BookDto dto)
+    {
+        var lastbook = Seeds.Seeds.Books.LastOrDefault();
+
+        Book newBook = new Book
+        {
+            Id = lastbook?.Id + 1 ?? 1,
+            Title = dto.Title,
+            AuthorsId = dto.AuthorsId
+        };
+
+        Seeds.Seeds.Books.Add(newBook);
+        
+        return CreatedAtAction(nameof(GetOne), new { id = newBook.Id }, newBook);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var existBook = Seeds.Seeds.Books.FirstOrDefault(x => x.Id == id);
+        if (existBook is null) return NotFound();
+
+        Seeds.Seeds.Books.Remove(existBook);
+
+        return NoContent();
+    }
+}
---
+++ Library.Api/Dto/BookDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Api.Dto;
+
+public class BookDto
+{
+    [Required]
+    public String Title { get; set; }
+    [Required]
+    public List<int> AuthorsId { get; set; }
+}
